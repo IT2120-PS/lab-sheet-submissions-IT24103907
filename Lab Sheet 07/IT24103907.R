4a31f3e36ef870792e2113a26e4b7a5e335ccd68
IT24103907 <it24103907@my.sliit.lk>
2025-09-17 19:46:31 +0530

    IT24103907 PS Lab 07

---
+++ Lab Sheet 07/IT24103907.R
@@ -0,0 +1,15 @@
+setwd("C:\\Users\\DELL\\Desktop\\IT24103907")
+getwd()
+
+#Question 1
+punif(25,min = 0,max = 40,lower.tail = TRUE)
+punif(10,min = 0,max = 40,lower.tail = TRUE)
+
+#Question 2
+pexp(2,rate = 1/3,lower.tail = TRUE)
+
+#Question 3.i
+pnorm(130, mean = 100, sd = 15, lower.tail = FALSE)
+
+# Question 3.ii
+qnorm(0.95, mean = 100, sd = 15, lower.tail = TRUE)
